0084d17ffd0e0742c84fb3e01f58968c2269f628
Atmesh Mishra <atmesh1.mishra@ril.com>
2017-05-18 13:25:43 +0530

    added db and permission

--- manifests/init.pp
+++ manifests/init.pp
@@ -18,5 +18,10 @@ class puppet_mysql {
   root_password           => 'pass',
   remove_default_accounts => true,
 }
-
+mysql::db { 'graphite':
+    user     => root,
+    password => 'pass',
+    grant    => [ 'SELECT' , 'INSERT' , 'UPDATE' , 'DELETE' , 'DROP' , 'CREATE' ,
+                  'ALTER' , 'CREATE VIEW' , 'SHOW VIEW' , 'TRIGGER' , 'LOCK TABLES' ],
+  }
 }
