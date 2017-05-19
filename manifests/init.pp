b37dce5c4531febac12d94a3268811563be7addd
Atmesh Mishra <atmesh1.mishra@ril.com>
2017-05-19 17:14:08 +0530

    added permission

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,17 +11,29 @@
 # Sample Usage:
 #
 class puppet_mysql {
-  
-  include '::mysql::server'
-  
+  # include '::mysql::server'
+
   class { '::mysql::server':
-  root_password           => 'pass',
-  remove_default_accounts => true,
-}
-mysql::db { 'graphite':
-    user     => root,
+    root_password           => 'pass',
+    remove_default_accounts => true,
+    override_options        =>  { 'mysqld' => { 'bind-address' => '0.0.0.0' } }
+  } ->
+  mysql::db { 'graphite':
+    user     => foo,
     password => 'pass',
-    grant    => [ 'SELECT' , 'INSERT' , 'UPDATE' , 'DELETE' , 'DROP' , 'CREATE' ,
-                  'ALTER' , 'CREATE VIEW' , 'SHOW VIEW' , 'TRIGGER' , 'LOCK TABLES' ],
-  }
+    grant    => [
+      'SELECT',
+      'INSERT',
+      'UPDATE',
+      'DELETE',
+      'DROP',
+      'CREATE',
+      'ALTER',
+      'CREATE VIEW',
+      'SHOW VIEW',
+      'TRIGGER',
+      'LOCK TABLES'],
+  } ->
+  exec { "/usr/bin/mysql --defaults-extra-file=/root/.my.cnf --database=mysql -e \"GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'pass' WITH GRANT OPTION;\""
+  : provider => shell }
 }
